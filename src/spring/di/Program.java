26bf3a0d3e097bd768aa1657bf7304743fad79ee
ddonggirim <choibu02222@naver.com>
2024-12-15 18:36:34 +0900

    spring property

---
+++ src/spring/di/Program.java
@@ -0,0 +1,25 @@
+package spring.di;
+
+import spring.di.entity.ChoibuExam;
+import spring.di.entity.Exam;
+import spring.di.ui.ExamConsole;
+import spring.di.ui.GridExamConsole;
+import spring.di.ui.InlineExamConsole;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/*
+		Exam exam = new ChoibuExam();
+		ExamConsole console = new GridExamConsole(exam);
+		
+		console.setExam(exam);
+		*/
+		
+		
+		ExamConsole console = ;
+		console.print();
+	}
+
+}
