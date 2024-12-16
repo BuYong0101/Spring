6b05619cddbf68d331b33e89304cf18ba53fce59
ddonggirim <choibu02222@naver.com>
2024-12-16 18:13:17 +0900

    xml,property

--- src/spring/di/entity/ChoibuExam.java
+++ src/spring/di/entity/ChoibuExam.java
@@ -7,6 +7,50 @@ public class ChoibuExam implements Exam {
 	private int math;
 	private int com;
 	
+	public ChoibuExam() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ChoibuExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+
+	
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
 	@Override
 	public int total() {
 		// TODO Auto-generated method stub
@@ -19,4 +63,9 @@ public class ChoibuExam implements Exam {
 		return total() / 4.0f;
 	}
 
+	@Override
+	public String toString() {
+		return "ChoibuExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	}
+	
 }
